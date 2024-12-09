48d499d164be14ad852238f658f6fea3cc6d465f
cbjoed <cbjoedstrup@gmail.com>
2024-12-09 13:35:55 +0100

    decimal i product

--- AuctionServiceAPI/obj/Debug/net8.0/AuctionServiceAPI.AssemblyInfo.cs
+++ AuctionServiceAPI/obj/Debug/net8.0/AuctionServiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fdc2b89d5fa6ee58af9b91e362d71107d38959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8bcd0f4ab448f399142cf6f66a0752651591e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
