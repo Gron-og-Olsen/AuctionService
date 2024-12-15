e360a5082c7bbfab245e2b10fc8c9985e5915f2f
KongBerg <nikolaj.kongsgaard.berg@gmail.com>
2024-12-15 13:30:28 +0100

    List Auctions burde trække fra mongo nu?!!!

--- AuctionServiceAPI/Pages/Pages/AuctionsList/AuctionsList.cshtml.cs
+++ AuctionServiceAPI/Pages/Pages/AuctionsList/AuctionsList.cshtml.cs
@@ -1,18 +1,24 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Http;
 using Models;
+using MongoDB.Driver;
 using System.Net.Http;
 using System.Net.Http.Json;
-
 
 namespace MyApp.Namespace
 {
     public class CatalogListModel : PageModel
     {
         private readonly IHttpClientFactory? _clientFactory = null;
+        private readonly IMongoClient _mongoClient;
         public List<Product>? Products { get; set; }
-        public CatalogListModel(IHttpClientFactory clientFactory)
-        => _clientFactory = clientFactory;
+
+        public CatalogListModel(IHttpClientFactory clientFactory, IMongoClient mongoClient)
+        {
+            _clientFactory = clientFactory;
+            _mongoClient = mongoClient;
+        }
+
         public void OnGet()
         {
             using HttpClient? client = _clientFactory?.CreateClient("gateway");
@@ -24,6 +30,21 @@ namespace MyApp.Namespace
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+
+            // Ekstra kode for at hente data fra MongoDB
+            var database = _mongoClient.GetDatabase("AuctionDB");
+            var collection = database.GetCollection<Product>("AuctionCollection");
+            var productsFromDb = collection.Find(FilterDefinition<Product>.Empty).ToList();
+
+            // Eksempel på at kombinere data fra API og MongoDB
+            if (Products != null)
+            {
+                Products.AddRange(productsFromDb);
+            }
+            else
+            {
+                Products = productsFromDb;
             }
         }
     }
