49424ffd1bef1be6cc2482d005c7a0d7ef5fcc42
KongBerg <nikolaj.kongsgaard.berg@gmail.com>
2024-12-15 12:29:30 +0100

    HTML file Tilføjet +  pakker

--- AuctionServiceAPI/obj/Debug/net8.0/AuctionServiceAPI.AssemblyInfo.cs
+++ AuctionServiceAPI/obj/Debug/net8.0/AuctionServiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390fc5555c44ad5fc3899b1c52e94c9cad35eb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1d0966762ae22963d52f9fcdfa8c8e5bfc08fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
